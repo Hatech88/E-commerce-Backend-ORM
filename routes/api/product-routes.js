49729ada90dc41255b4a2e912be3f556ff6a6ce4
Harris Alshareef <hatech88@gmail.com>
2022-06-12 14:58:15 -0400

    Routes Testing Updates

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -55,14 +55,35 @@ router.get("/:id", (req, res) => {
 
 // create new product
 router.post("/", (req, res) => {
+	// Product.create(req.body).then((newProduct) => {
+	//   res.json(newProduct);
+	// })
+	// .catch((err) => {
+	//   res.json(err);
+	// });
 	/* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+	  {
+		product_name: "Basketball",
+		price: 200.00,
+		stock: 3,
+		tagIds: [1, 2, 3, 4]
+	  }
+	*/
+
+	// router.post('/', async (req, res) => {
+	// 	// create a new product or update
+	// 	await Product.create(req.body)
+	// 		  .then((newProduct) => res.status(200).json(newProduct))
+	// 		  .catch((err) => {
+	// 			  console.log(err);
+	// 			  res.status(400).json(err);
+	// 		  });
+	//   });
+
+
+
+
+
 	Product.create(req.body)
 		.then((product) => {
 			// if there's product tags, we need to create pairings to bulk create in the ProductTag model
